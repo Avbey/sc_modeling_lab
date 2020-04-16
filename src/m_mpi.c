de3b2dda2fa09ea9eed09f2dca88ec4a7b109bde
Avbey <hbyf500350>
2020-04-16 09:59:41 +0300

    added comments to m_mpi

--- src/m_mpi.c
+++ src/m_mpi.c
@@ -7,6 +7,7 @@
 #include "matrix.h"
 
 complex double *flatten(matrix_struct *m) {
+    /*Converts matrix into 1D array*/
     complex double *matrix = malloc((m->rows * m->cols) * sizeof(complex double));
     for (int i = 0; i < m->rows; ++i) {
         memcpy(matrix + (i * m->cols), m->data[i], m->cols * sizeof(complex double));
@@ -15,23 +16,20 @@ complex double *flatten(matrix_struct *m) {
 }
 
 int main(int argc, char *argv[]) {
-    int dims[4];
-    complex double *flat_a = NULL;
-    complex double *flat_b = NULL;
-    complex double *flat_final = NULL;
+    int dims[4];    // stores A and B dimensions
+    complex double *flat_a = NULL;  // stores 1D A
+    complex double *flat_b = NULL;  // stores 1D B
+    complex double *flat_result = NULL;  // stores 1D C
     int worker_count, rank;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &worker_count);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-//    double startMPI = MPI_Wtime();
 
     if (rank == 0) {
         matrix_struct matrixA, matrixB;
 
-//        randomize_matrix(&matrixA, 1200);
-//        randomize_matrix(&matrixB, 1200);
         matrix_from_std(&matrixA);
         matrix_from_std(&matrixB);
 
@@ -49,12 +47,13 @@ int main(int argc, char *argv[]) {
 
     MPI_Bcast(&dims, 4, MPI_INT, 0, MPI_COMM_WORLD);
 
+    // Assigning sizes of 1D matrices
     int size_a = dims[0] * dims[1];
     int size_b = dims[2] * dims[3];
     int size_res = dims[1] * dims[3];
 
     if(rank == 0) {
-        flat_final = malloc(size_res * sizeof(complex double));
+        flat_result = malloc(size_res * sizeof(complex double));
     } else {
         flat_a = malloc(size_a * sizeof(complex double));
         flat_b = malloc(size_b * sizeof(complex double));
@@ -67,6 +66,7 @@ int main(int argc, char *argv[]) {
         MPI_Abort(MPI_COMM_WORLD, 0);
     }
 
+    // Dividing result matrix by rows to be executed by worker_count processes
     int count = dims[1] / worker_count;
     int remainder = dims[1] % worker_count;
     int start_row, end_row;
@@ -81,33 +81,28 @@ int main(int argc, char *argv[]) {
     int part_count = (end_row - start_row + 1) * dims[3];
     complex double *part_matrix = calloc(part_count, sizeof(complex double));
 
-    int position = 0;
-
+    int pos = 0;
     for (int i = start_row; i <= end_row; ++i) {
         for (int j = 0; j < dims[3]; ++j) {
             for (int k = 0; k < dims[0]; ++k) {
-                part_matrix[position] += conj(flat_a[(k * dims[1] + i)]) * flat_b[(k * dims[3] + j)];
+                part_matrix[pos] += conj(flat_a[(k * dims[1] + i)]) * flat_b[(k * dims[3] + j)];
             }
-            position++;
+            pos++;
         }
     }
 
     free(flat_a);
     free(flat_b);
 
-    MPI_Gather(part_matrix, part_count, MPI_C_DOUBLE_COMPLEX, flat_final, part_count, MPI_C_DOUBLE_COMPLEX, 0, MPI_COMM_WORLD);
+    MPI_Gather(part_matrix, part_count, MPI_C_DOUBLE_COMPLEX, flat_result, part_count, MPI_C_DOUBLE_COMPLEX, 0, MPI_COMM_WORLD);
 
-//    double endMPI = MPI_Wtime();
-//    double GFlops = 12.0e-9*(dims[0])*(dims[0])*(dims[0])/(endMPI - startMPI);
-//    printf("%f\n", GFlops);
-
-    // print result
+    // Printing result to stdout
     if (rank == 0){
         int size = dims[1] * dims[3];
         int i = 0;
         printf("%d %d\n", dims[1], dims[3]);
         while (i < size) {
-            printf("%f%+fi ", creal(flat_final[i]), cimag(flat_final[i]));
+            printf("%f%+fi ", creal(flat_result[i]), cimag(flat_result[i]));
             i++;
 
             if (i % dims[3] == 0)
@@ -116,7 +111,7 @@ int main(int argc, char *argv[]) {
     }
 
     free(part_matrix);
-    free(flat_final);
+    free(flat_result);
 
     MPI_Finalize();
     exit(EXIT_SUCCESS);
